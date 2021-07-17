9f4b2161b38795d739aeeadb28273acccdd8e8b2
Hitesh Chhillar <hitesh.chhillar16@gmail.com>
2021-07-17 19:02:50 -0400

    Update Utility.cs

--- Util/Utility.cs
+++ Util/Utility.cs
@@ -29,12 +29,9 @@ namespace SeleniumProjectShell
         private ArgTypes(string value) { Value = value; }
         public string Value { get; set; }
         public static ArgTypes Page { get { return new ArgTypes("pg"); } }
-        public static ArgTypes FirstName { get { return new ArgTypes("rfn"); } }
-        public static ArgTypes LastName { get { return new ArgTypes("rln"); } }
         public static ArgTypes Username { get { return new ArgTypes("un"); } }
         public static ArgTypes Password { get { return new ArgTypes("pd"); } }
         public static ArgTypes Environment { get { return new ArgTypes("env"); } }
-        public static ArgTypes Facility { get { return new ArgTypes("fc"); } }
         public static ArgTypes ExcelData { get { return new ArgTypes("excelData"); } }
 
     }
