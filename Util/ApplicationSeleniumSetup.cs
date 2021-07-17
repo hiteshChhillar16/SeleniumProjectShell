3e234368dc253523880bdc34f504c5b88e70feb3
Hitesh Chhillar <hitesh.chhillar16@gmail.com>
2021-07-17 18:54:39 -0400

    Update ApplicationSeleniumSetup.cs

--- Util/ApplicationSeleniumSetup.cs
+++ Util/ApplicationSeleniumSetup.cs
@@ -24,71 +24,16 @@ namespace SeleniumProjectShell
             }
 
             appSettings.WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
-            IWebElement username = appSettings.WebDriver.FindElement(By.Id("j_username"));
+            IWebElement username = appSettings.WebDriver.FindElement(By.Id("username"));
             username.SendKeys(appSettings.Username);
 
-            IWebElement password = appSettings.WebDriver.FindElement(By.Id("j_password"));
+            IWebElement password = appSettings.WebDriver.FindElement(By.Id("password"));
             password.SendKeys(appSettings.Password);
 
             IWebElement signIn;
-            if (appSettings.Environment.Contains("qa17"))
-            {
-                signIn = appSettings.WebDriver.FindElement(By.Id("loginSubmit"));
-            }
-            else
-            {
-                signIn = appSettings.WebDriver.FindElement(By.Id("loginbtn"));
-            }
+            signIn = appSettings.WebDriver.FindElement(By.Id("loginButton"));
 
             signIn.Click();
-        }
-
-        public static void SetFacility(Setting appSettings)
-        {
-            string facilityTitleText;
-            IWebElement facilityTitleName;
-            try
-            {
-                facilityTitleName = appSettings.WebDriver.FindElement(By.XPath("/html/body/div/div[3]/span[3]/a"));
-                facilityTitleText = facilityTitleName.Text;
-            }
-            catch (Exception ex)
-            {
-                facilityTitleText = "";
-            }
-            if (facilityTitleText == "" || !appSettings.Facility.Contains(facilityTitleText))
-            {
-
-                //If different facility is selected then clean up textbox and search for our facility
-                IWebElement facilitySearchTextbox = appSettings.WebDriver.FindElement(By.Name("facility_name"));
-                facilitySearchTextbox.Clear();
-                facilitySearchTextbox.SendKeys(appSettings.Facility);
-                IWebElement searchFacilityButton = appSettings.WebDriver.FindElement(By.Name("search"));
-                searchFacilityButton.Click();
-                IWebElement facilityItem = appSettings.WebDriver.FindElement(By.PartialLinkText(appSettings.Facility));
-                facilityItem.Click();
-            }
-
-            SetResident(appSettings);
-        }
-
-        public static void SetResident(Setting appSettings)
-        {
-            appSettings.WebDriver.Navigate().GoToUrl(appSettings.Environment + "/Zion?zionpagealias=SELECTPATIENT");
-
-            IWebElement lastNameResidentSearch = appSettings.WebDriver.FindElement(By.Id("LastName"));
-            IWebElement firstNameResidentSearch = appSettings.WebDriver.FindElement(By.Name("FirstName"));
-
-            lastNameResidentSearch.Clear();
-            firstNameResidentSearch.Clear();
-            lastNameResidentSearch.SendKeys(appSettings.ResidentLastName);
-            firstNameResidentSearch.SendKeys(appSettings.ResidentFirstName);
-            IWebElement searchResident = appSettings.WebDriver.FindElement(By.Name("Search"));
-            searchResident.Click();
-
-            IWebElement residentElement = appSettings.WebDriver.FindElement(By.XPath("//*[@id='patientList']/table/tbody/tr/td[1]/a"));
-
-            residentElement.Click();
         }
     }
 }
